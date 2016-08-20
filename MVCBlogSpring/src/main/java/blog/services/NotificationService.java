190c01b9a5a81189329362f024a52a1cc4d6f514
vonrepiks <ico_skipernov@abv.bg>
2016-08-21 01:28:31 +0300

    SpringMVC Blog Login Register

--- MVCBlogSpring/src/main/java/blog/services/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package blog.services;
-
-/**
- * Created by Hristo on 05.08.2016 г..
- */
-public interface NotificationService {
-    void addInfoMessage(String msg);
-    void addErrorMessage(String msg);
-}
